ec80b912281221cd47df0719ce6065c607e06de8
Sorigh <84138511+Sorigh12@users.noreply.github.com>
2024-01-27 14:30:45 -0500

    Add files via upload
    Creating a Tenzies game

---
+++ Tenzies game/App.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Die from "./Die"
+
+/**
+ * Challenge:
+ * 
+ * Create state to hold our array of numbers. (Initialize
+ * the state by calling our `allNewDice` function so it 
+ * loads all new dice as soon as the app loads)
+ * 
+ * Map over the state numbers array to generate our array
+ * of Die elements and render those in place of our
+ * manually-written 10 Die elements.
+ */
+
+export default function App() {
+    const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    
+    const diceElements = dice.map(die => <Die value={die} />)
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+        </main>
+    )
+}
